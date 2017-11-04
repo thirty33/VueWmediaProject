d31c890863f3026a6e49d450d1d9aeee41e71083
Thirty33 <joelsuarez.1101@gmail.com>
2017-11-03 19:14:18 -0500

    css dinamico con Css Modules de Vuejs

--- src/App.vue
+++ src/App.vue
@@ -19,6 +19,11 @@
       <li><a href="">enlace</a> </li>
       <li><a href="">enlace</a> </li>
     </ul>
+
+
+
+    <h1 :class="[$style.rojo]" >Hola css Modules</h1>
+    <h1 :class="{[$style.subrayado] : subrayo}" >Hola css Modules</h1>
   </div>
 </template>
 
@@ -26,19 +31,32 @@
   // import persona from './components/persona.vue'
   // import elemento from './components/elemento.vue'
   import Tareas from './components/tareas.vue'
-export default {
-  components: { Tareas },
   // components: { elemento },
   // components: { persona },
+export default {
+  components: { Tareas },
+  data(){
+    return {
+      subrayo: false
+    }
+  }
   
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" module>
 
 
   li a {
     color: black;
+  }
+
+  .subrayado {
+    text-decoration: underline;
+  }
+
+  .rojo {
+    color: red;
   }
 </style>
   
